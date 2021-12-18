f70ded4fba0b0206f95e2255040497781fe959db
christophe jeunesse <christophe.jeunesse@orange.com>
2021-12-18 23:26:41 +0100

    [Rust] Day 16 - Parser avec tous les contrôles d'erreur

--- rust/src/day16.rs
+++ rust/src/day16.rs
@@ -1,6 +1,10 @@
 use crate::utils::Part;
-use std::str::Chars;
+use std::{
+    cmp::{max, min},
+    str::Chars,
+};
 
+#[derive(PartialEq, Eq, Debug)]
 enum Expr {
     Literal {
         version: u8,
@@ -39,24 +43,25 @@ enum Expr {
     },
 }
 
+#[derive(Debug)]
 enum ParseExprError {
     UnknownOperation,
-    EmptyInput,
     NotEnoughChars,
     BadHexChar,
     BadSubExpressionCount,
     GuardReached,
+    BadGuardState,
 }
 
 struct ReaderState<'a> {
     curr_str: Chars<'a>,
-    curr_byte: u16,
+    curr_byte: u32,
     insert_pos: u8,
     bits_read: u32,
     guards: Vec<u32>,
 }
 
-fn hex_to_str(chr: char) -> Result<u16, ParseExprError> {
+fn hex_to_str(chr: char) -> Result<u32, ParseExprError> {
     match chr {
         '0' => Ok(0),
         '1' => Ok(1),
@@ -89,27 +94,29 @@ impl<'a> ReaderState<'a> {
         }
     }
 
-    fn read_n_bits(&mut self, nb: u8) -> Result<u16, ParseExprError> {
+    fn read_n_bits(&mut self, nb: u8) -> Result<u32, ParseExprError> {
         while self.insert_pos < nb {
             self.decode_next_byte()?
         }
-        let unshifted_mask: u16 = (1 << nb) - 1;
-        let mask = unshifted_mask << (16 - nb);
-        let result = ((self.curr_byte & mask) >> (16 - nb));
-        self.consume_bits(nb as u32);
+        let unshifted_mask: u32 = (1 << nb) - 1;
+        let mask = unshifted_mask << (32 - nb);
+        let result = (self.curr_byte & mask) >> (32 - nb);
+        self.consume_bits(nb as u32)?;
         return Ok(result);
     }
 
-    pub fn add_guard(&mut self, size: u16) {
-        self.guards.push(size as u32 + self.bits_read)
+    pub fn add_guard(&mut self, size: u32) {
+        self.guards.push(size + self.bits_read)
     }
 
-    pub fn remove_guard(&mut self) {
+    pub fn remove_guard(&mut self) -> Result<(), ParseExprError> {
         if let Some(value) = self.guards.pop() {
-            if (self.bits_read < value) {
-                self.consume_bits(value - self.bits_read);
+            if self.bits_read < value {
+                self.consume_bits(value - self.bits_read)?;
             }
+            return Ok(());
         }
+        return Err(ParseExprError::BadGuardState);
     }
 
     pub fn consume_bits(&mut self, nb: u32) -> Result<(), ParseExprError> {
@@ -119,21 +126,27 @@ impl<'a> ReaderState<'a> {
             self.consume_bits(self.insert_pos as u32)?;
             self.decode_next_byte()?
         }
+        self.check_guard()?;
         self.curr_byte <<= remaining_to_consume as u8;
         self.insert_pos -= remaining_to_consume as u8;
+        self.bits_read += remaining_to_consume;
         Result::Ok(())
     }
 
     fn decode_next_byte(&mut self) -> Result<(), ParseExprError> {
-        if self.guards.iter().any(|max| (max) < &self.bits_read) {
-            return Err(ParseExprError::GuardReached);
-        }
+        self.check_guard()?;
         let char = self.curr_str.next().ok_or(ParseExprError::NotEnoughChars)?;
         let value = hex_to_str(char)?;
-        self.curr_byte |= value << (16 - self.insert_pos - 4);
+        self.curr_byte |= value << (32 - self.insert_pos - 4);
         self.insert_pos += 4;
-        self.bits_read += 1;
         Result::Ok(())
+    }
+
+    fn check_guard(&self) -> Result<(), ParseExprError> {
+        if self.guards.iter().any(|max| (max) <= &self.bits_read) {
+            return Err(ParseExprError::GuardReached);
+        }
+        return Ok(());
     }
 }
 
@@ -158,11 +171,11 @@ fn parse_variable_with_length(reader_state: &mut ReaderState) -> Result<Vec<Expr
         let next_expr = parse_expr(reader_state);
         match next_expr {
             Ok(expr) => res.push(expr),
-            Err(ParseExprError::NotEnoughChars) => break,
+            Err(ParseExprError::GuardReached) => break,
             Err(error) => return Err(error),
         }
     }
-    reader_state.remove_guard();
+    reader_state.remove_guard()?;
     return Ok(res);
 }
 
@@ -171,11 +184,10 @@ fn parse_variable_with_number_of_sub_expr(
 ) -> Result<Vec<Expr>, ParseExprError> {
     let mut res: Vec<Expr> = vec![];
     let length = reader_state.read_n_bits(11)?;
-    while (res.len() < (length as usize)) {
+    while res.len() < (length as usize) {
         let next_expr = parse_expr(reader_state);
         match next_expr {
             Ok(expr) => res.push(expr),
-            Err(ParseExprError::NotEnoughChars) => break,
             Err(error) => return Err(error),
         }
     }
@@ -230,21 +242,33 @@ fn parse_expr(reader_state: &mut ReaderState) -> Result<Expr, ParseExprError> {
         }),
         5 => {
             let (a, b) = parse_tuple(reader_state)?;
-            Ok(Expr::Gt { version, a:Box::new(a), b:Box::new(b) })
+            Ok(Expr::Gt {
+                version,
+                a: Box::new(a),
+                b: Box::new(b),
+            })
         }
         6 => {
             let (a, b) = parse_tuple(reader_state)?;
-            Ok(Expr::Lt { version, a:Box::new(a), b:Box::new(b) })
+            Ok(Expr::Lt {
+                version,
+                a: Box::new(a),
+                b: Box::new(b),
+            })
         }
-        7=> {
+        7 => {
             let (a, b) = parse_tuple(reader_state)?;
-            Ok(Expr::Eq{ version, a:Box::new(a), b:Box::new(b) })
+            Ok(Expr::Eq {
+                version,
+                a: Box::new(a),
+                b: Box::new(b),
+            })
         }
         _ => Err(ParseExprError::UnknownOperation),
     };
 }
 
-fn parse_line(line: &String) -> Result<Expr, ParseExprError> {
+fn parse_line(line: &str) -> Result<Expr, ParseExprError> {
     return Ok(parse_expr(&mut ReaderState::new(line))?);
 }
 
@@ -255,27 +279,161 @@ fn parse(lines: &Vec<String>) -> Result<Expr, ParseExprError> {
         .unwrap_or(Result::Err(ParseExprError::UnknownOperation));
 }
 
-fn sum_version_content(expr:Expr)->u32{
+fn calc_version(expr: &Expr) -> u32 {
     return match expr {
-        Expr::Literal { version, .. } => version as u32,
-        Expr::Eq
-    }
+        Expr::Literal { version, .. } => *version as u32,
+        Expr::Eq { version, a, b } | Expr::Gt { version, a, b } | Expr::Lt { version, a, b } => {
+            *version as u32 + calc_version(a.as_ref()) + calc_version(b.as_ref())
+        }
+        Expr::Sum { version, args }
+        | Expr::Mul { version, args }
+        | Expr::Min { version, args }
+        | Expr::Max { version, args } => args
+            .iter()
+            .fold(*version as u32, |sum, arg| sum + calc_version(arg)),
+    };
 }
 
-fn sum_version(expr:Expr)->u32{
-    
+fn evaluate(expr: &Expr) -> u64 {
+    return match expr {
+        Expr::Literal { value, .. } => *value,
+        Expr::Eq { a, b, .. } => {
+            if evaluate(a.as_ref()) == evaluate(b.as_ref()) {
+                1
+            } else {
+                0
+            }
+        }
+        Expr::Gt { a, b, .. } => {
+            if evaluate(a.as_ref()) > evaluate(b.as_ref()) {
+                1
+            } else {
+                0
+            }
+        }
+        Expr::Lt { a, b, .. } => {
+            if evaluate(a.as_ref()) < evaluate(b.as_ref()) {
+                1
+            } else {
+                0
+            }
+        }
+        Expr::Sum { args, .. } => args.iter().fold(0, |sum, val| sum + evaluate(val)),
+        Expr::Mul { args, .. } => args.iter().fold(1, |mul, val| mul * evaluate(val)),
+        Expr::Min { args, .. } => args
+            .iter()
+            .fold(std::u64::MAX, |min_val, val| min(min_val, evaluate(val))),
+        Expr::Max { args, .. } => args
+            .iter()
+            .fold(0 as u64, |max_val, val| max(max_val, evaluate(val))),
+    };
 }
 
 pub fn puzzle(part: &Part, lines: &Vec<String>) {
     let expr_res = parse(lines);
-    let expr =expr_res.unwrap();
+    let expr = expr_res.unwrap();
     match part {
         Part::Part1 => {
-
-            println!("Result {}", "nothing")
+            let result = calc_version(&expr);
+            println!("Result Version {}", result)
         }
         Part::Part2 => {
-            println!("Result {}", "nothing");
+            let result = evaluate(&expr);
+            println!("Result Eval {}", result);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_parse_literal() {
+        let parse_res = parse_line("D2FE28").unwrap();
+        assert_eq!(
+            parse_res,
+            Expr::Literal {
+                version: 6,
+                value: 2021
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_max_nb_sub_packets() {
+        let parse_res = parse_line("EE00D40C823060").unwrap();
+        assert_eq!(
+            parse_res,
+            Expr::Max {
+                version: 7,
+                args: vec![
+                    Expr::Literal {
+                        version: 2,
+                        value: 1
+                    },
+                    Expr::Literal {
+                        version: 4,
+                        value: 2
+                    },
+                    Expr::Literal {
+                        version: 1,
+                        value: 3
+                    }
+                ]
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_lt_variable_length() {
+        let parse_res = parse_line("38006F45291200").unwrap();
+        assert_eq!(
+            parse_res,
+            Expr::Lt {
+                version: 1,
+                a: Box::new(Expr::Literal {
+                    version: 6,
+                    value: 10
+                }),
+                b: Box::new(Expr::Literal {
+                    version: 2,
+                    value: 20
+                })
+            }
+        );
+    }
+
+    #[test]
+    fn test_sum_version_packets() {
+        assert_eq!(calc_version(&parse_line("8A004A801A8002F478").unwrap()), 16);
+        assert_eq!(
+            calc_version(&parse_line("620080001611562C8802118E34").unwrap()),
+            12
+        );
+        assert_eq!(
+            calc_version(&parse_line("C0015000016115A2E0802F182340").unwrap()),
+            23
+        );
+        assert_eq!(
+            calc_version(&parse_line("A0016C880162017C3686B18A3D4780").unwrap()),
+            31
+        )
+    }
+
+    #[test]
+    fn test_calc_packets() {
+        assert_eq!(evaluate(&parse_line("C200B40A82").unwrap()), 3);
+        assert_eq!(evaluate(&parse_line("04005AC33890").unwrap()), 54);
+        assert_eq!(evaluate(&parse_line("880086C3E88112").unwrap()), 7);
+        assert_eq!(evaluate(&parse_line("CE00C43D881120").unwrap()), 9);
+        assert_eq!(evaluate(&parse_line("D8005AC2A8F0").unwrap()), 1);
+        assert_eq!(evaluate(&parse_line("F600BC2D8F").unwrap()), 0);
+        assert_eq!(evaluate(&parse_line("9C005AC2F8F0").unwrap()), 0);
+        assert_eq!(
+            evaluate(&parse_line("9C0141080250320F1802104A08").unwrap()),
+            1
+        );
     }
 }
